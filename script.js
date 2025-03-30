33d0af5fb57f36b895a1d11adefc8364e335d27c
Mohammad Adnan <65945728+adnandotdev@users.noreply.github.com>
2025-03-30 21:25:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 //your JS code here. If required.
+let button = document.getElementById("btn")
+let output = document.getElementById("output")
+button.addEventListener("click", () => {
+	let inputNumber = document.getElementById("ip").value
+	 return new Promise((resolve) => {
+		setTimeout(() => {
+			output.textContent = "Result: " + inputNumber
+			resolve(inputNumber)
+		}, 2000)
+	})
+	.then((num) => {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				let outputNum = num*2
+				output.textContent = "Result" + outputNum
+				reslove(outputNum)
+			}, 2000)
+		})
+	})
+	.then((num) => {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				let outputNum = num-3
+				output.textContent = "Result" + outputNum
+				reslove(outputNum)
+			}, 1000)
+		})
+	})
+	.then((num) => {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				let outputNum = num/2
+				output.textContent = "Result" + outputNum
+				reslove(outputNum)
+			}, 1000)
+		})
+	})
+	.then((num) => {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				let outputNum = num+10
+				output.textContent = "Final Result" + outputNum
+				reslove(outputNum)
+			}, 1000)
+		})
+	})
+})
